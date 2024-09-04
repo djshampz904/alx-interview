c7f4ffeda65d2baf868191dbc6e87f754228e56d
djshampz904 <martin.kimani@strathmore.edu>
2024-09-04 15:18:53 +0300

    interview

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,48 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+if (process.argv.length !== 3) {
+    console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+    process.exit(1);
 }
+
+const movieId = process.argv[2];
+
+function fetchCharacter(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else if (response.statusCode !== 200) {
+                reject(new Error(`HTTP error! status: ${response.statusCode}`));
+            } else {
+                resolve(JSON.parse(body).name);
+            }
+        });
+    });
+}
+
+request(`${API_URL}/films/${movieId}/`, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        return;
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('HTTP error! status:', response.statusCode);
+        return;
+    }
+
+    const film = JSON.parse(body);
+    const characterPromises = film.characters.map(fetchCharacter);
+
+    Promise.all(characterPromises)
+        .then(names => {
+            names.forEach(name => console.log(name));
+        })
+        .catch(error => {
+            console.error('Error fetching characters:', error);
+        });
+});
